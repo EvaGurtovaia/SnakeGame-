7d3db0c90226617c88fb2bb8bf94401083cb58ad
Eva Gurtovaia <34386254+EvaGurtovaia@users.noreply.github.com>
2021-07-15 16:15:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+//const { classDeclaration } = require("babel-types");
+
+const grid = document.querySelector(".grid");
+const startButton = document.getElementById("start");
+const score = document.getElementById("score");
+let squares = [];
+let currentSnake = [2, 1, 0];
+let direction = 1;
+
+function createGrid() {
+    for (let i = 0; i < 100; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        grid.appendChild(square);
+        squares.push(square);
+    }
+}
+createGrid();
+
+currentSnake.forEach((index) => squares[index].classList.add("snake"));
+
+function move() {
+    let tail = currentSnake.pop();
+    squares[tail].classList.remove("snake");
+    currentSnake.unshift(currentSnake[0] + direction);
+    squares[currentSnake[0]].classList.add("snake");
+}
+move();
+
+const timerID = setInterval(move, 1000);
+//clearInterval(timerID);
