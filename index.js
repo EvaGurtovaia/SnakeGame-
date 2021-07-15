f7c17e21eeaa6879ebc0f2dc5ca027afbb25c745
Eva Gurtovaia <34386254+EvaGurtovaia@users.noreply.github.com>
2021-07-15 17:35:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
+let width = 10;
 
 function createGrid() {
     for (let i = 0; i < 100; i++) {
@@ -29,3 +30,49 @@ move();
 
 const timerID = setInterval(move, 1000);
 //clearInterval(timerID);
+
+// function control(event) {
+//     if (event.key === 39) {
+//         direction = 1;
+//     } else if (event.key === 38) {
+//         direction = -width;
+//     } else if (event.key === 40) {
+//         direction = -1;
+//     } else if (event.key === 37) {
+//         direction = +width;
+//     }
+// }
+
+window.addEventListener(
+    "keydown",
+    function (event) {
+        if (event.defaultPrevented) {
+            return; // Do nothing if the event was already processed
+        }
+
+        switch (event.key) {
+            case "Down":
+            case "ArrowDown":
+                direction = +width;
+                break;
+            case "Up":
+            case "ArrowUp":
+                direction = +width;
+                break;
+            case "Left":
+            case "ArrowLeft":
+                direction = -1;
+                break;
+            case "Right":
+            case "ArrowRight":
+                direction = 1;
+                break;
+            default:
+                return; // Quit when this doesn't handle the key event.
+        }
+
+        // Cancel the default action to avoid it being handled twice
+        event.preventDefault();
+    },
+    true
+);
