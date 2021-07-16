ca6114e2f8b3d874eb9a4693ddb190c2925c8066
Eva Gurtovaia <34386254+EvaGurtovaia@users.noreply.github.com>
2021-07-16 16:01:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const score = document.getElementById("score");
 let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
-let width = 10;
+const width = 10;
 
 function createGrid() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < width * width; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         grid.appendChild(square);
@@ -21,6 +21,15 @@ createGrid();
 currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 function move() {
+    if (
+        (currentSnake[0] + width >= width * width && direction === width) || //snake hits the bottom
+        (currentSnake[0] % width === width - 1 && direction === 1) || //snake hits the right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //snake hits the left wall
+        (currentSnake[0] - width < width && direction === -width) || // snake hits the top
+        squares[currentSnake[0] + direction].classList.contains("snake")
+    )
+        return clearInterval(timerID);
+
     let tail = currentSnake.pop();
     squares[tail].classList.remove("snake");
     currentSnake.unshift(currentSnake[0] + direction);
